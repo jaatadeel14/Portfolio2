da4e01573354cf5f86311d05b51a4810280da64a
Adeel Shabbir <jaatadeel6@gmail.com>
2024-07-29 02:54:52 +0300

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,26 @@
 import React from 'react'
 import Navbar from './components/Navbar'
 import Home from './components/Home'
-
+import About from './components/About'
+import Portfolio from './components/Portfolio'
+import Experiance from './components/Experiance'
+import Footer from './components/Footer'
+import Contact from './components/Contact'
+import { Toaster } from 'react-hot-toast'
 function App() {
   return (
     <>
-    <Navbar/>
+   <div>
+   <Navbar/>
     <Home/>
+    <About/>
+    <Portfolio/>
+    <Experiance/>
+    <Contact/>
+    <Footer/>
+   </div>
+   <Toaster />
+
     </>
   )
 }
